70b8e36d575283a80f715b719626105a91c75dd5
ERBCosta <eltonr@gmail.com>
2022-06-07 09:50:39 -0300

    Criando o repositório para o exercício de lista de registro do curso Android Express.

---
+++ app/src/main/java/br/com/leton/androidexpressexerciciolistaderegistro/MainActivity.java
@@ -0,0 +1,13 @@
+package br.com.leton.androidexpressexerciciolistaderegistro;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
