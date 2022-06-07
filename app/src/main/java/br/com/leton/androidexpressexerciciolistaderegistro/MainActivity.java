5edccfec1c0978fc28216e116a340edc280f5188
ERBCosta <eltonr@gmail.com>
2022-06-07 14:14:57 -0300

    App não esta abrindo com esse código.

--- app/src/main/java/br/com/leton/androidexpressexerciciolistaderegistro/MainActivity.java
+++ app/src/main/java/br/com/leton/androidexpressexerciciolistaderegistro/MainActivity.java
@@ -1,13 +1,61 @@
 package br.com.leton.androidexpressexerciciolistaderegistro;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView rvTelaPrincipal;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        rvTelaPrincipal = findViewById(R.id.rv_tela_principal);
+        rvTelaPrincipal.setLayoutManager(new LinearLayoutManager(this));
+        MainAdapter adapter = new MainAdapter();
+        rvTelaPrincipal.setAdapter(adapter);
+    }
+
+    private class MainAdapter extends RecyclerView.Adapter<MainViewHolder> {
+
+        @NonNull
+        @Override
+        public MainViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+            return new MainViewHolder(getLayoutInflater().inflate(R.layout.item_principal, parent, false));
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull MainViewHolder holder, int position) {
+            holder.bind(position);
+
+        }
+
+        @Override
+        public int getItemCount() {
+            return 15;
+        }
+    }
+
+    private class MainViewHolder extends RecyclerView.ViewHolder {
+
+        public MainViewHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+
+        public void bind(int position){
+            TextView textTest = itemView.findViewById(R.id.texto_view_teste);
+            textTest.setText("teste de rolagem" + position);
+
+        }
     }
 }
